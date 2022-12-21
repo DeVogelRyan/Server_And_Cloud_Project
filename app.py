bf44c66c4d7ab9b32e1a2c1a154bae88181872e6
DeVogelRyan <devogelryan2@gmail.com>
2022-12-21 11:10:41 +0100

    added ID to url and added ability to see changed on the fly

--- app.py
+++ app.py
@@ -2,10 +2,9 @@ from flask import Flask
 
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello world!"
-
+@app.route("/<id>")
+def hello(id):
+    return "Returned the id: " + str(id)
 
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
