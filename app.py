20908f4c0842aab39dd3bb1917ca5015f12b71ac
DeVogelRyan <devogelryan2@gmail.com>
2022-12-22 13:36:34 +0100

    added default route

--- app.py
+++ app.py
@@ -2,10 +2,13 @@ from flask import Flask
 
 app = Flask(__name__)
 
-
 @app.route("/<id>")
-def hello(id):
+def getID(id):
     return "Returned the id: " + str(id)
 
+@app.route("/")
+def hello():
+    return "Hello world"
+    
 if __name__ == "__main__":
     app.run(debug=True, host="0.0.0.0")
