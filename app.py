c90341460588926074a275dc2599ef3ee5e7e26f
DeVogelRyan <devogelryan2@gmail.com>
2022-12-29 16:08:44 +0100

    removed db code in flask

--- app.py
+++ app.py
@@ -1,25 +1,10 @@
-from flask import Flask,render_template, request
-from flask_mysqldb import MySQL
+from flask import Flask
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'mysql'
-app.config['MYSQL_USER'] = 'root'
-app.config['MYSQL_PASSWORD'] = 'root'
-app.config['MYSQL_DB'] = 'mydata'
+@app.route("/<id>")
+def hello(id):
+    return "Returned the id: " + str(id)
 
-mysql = MySQL(app)
-
-with app.app_context():
-#Creating a connection cursor
-    cursor = mysql.connection.cursor()
-
-    #Executing SQL Statements
-    cursor.execute('''  CREATE TABLE friends (id INT, name VARCHAR(256), age INT, gender VARCHAR(3)); ''')
-    cursor.execute('''  INSERT INTO friends VALUES (1, 'Abhijeet', 32, 'm'); ''')
-
-    #Saving the Actions performed on the DB
-    mysql.connection.commit()
-
-    #Closing the cursor
-    cursor.close()
+if __name__ == "__main__":
+    app.run(debug=True, host="0.0.0.0")
