4d130bb5e753e6668596dfab12e98b134b6ca1f4
DeVogelRyan <devogelryan2@gmail.com>
2022-12-29 15:33:58 +0100

    added service

--- app.py
+++ app.py
@@ -3,7 +3,7 @@ from flask_mysqldb import MySQL
 
 app = Flask(__name__)
 
-app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_HOST'] = 'mysql'
 app.config['MYSQL_USER'] = 'root'
 app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'mydata'
