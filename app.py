c3a792f65a29705888981fa4b120d02768c1d5c6
DeVogelRyan <devogelryan2@gmail.com>
2022-12-29 14:35:12 +0100

    fixed python error

--- app.py
+++ app.py
@@ -1,23 +1,25 @@
 from flask import Flask,render_template, request
 from flask_mysqldb import MySQL
- 
+
 app = Flask(__name__)
- 
+
 app.config['MYSQL_HOST'] = 'localhost'
-app.config['MYSQL_USER'] = 'RyanProject'
-app.config['MYSQL_PASSWORD'] = 'Password!321'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = 'root'
 app.config['MYSQL_DB'] = 'mydata'
- 
+
 mysql = MySQL(app)
 
+with app.app_context():
 #Creating a connection cursor
-cursor = mysql.connection.cursor()
- 
-#Executing SQL Statements
-cursor.execute(''' INSERT INTO friends VALUES (4, 'Ryan', 20, 'm') ''')
- 
-#Saving the Actions performed on the DB
-mysql.connection.commit()
- 
-#Closing the cursor
-cursor.close()
+    cursor = mysql.connection.cursor()
+
+    #Executing SQL Statements
+    cursor.execute('''  CREATE TABLE friends (id INT, name VARCHAR(256), age INT, gender VARCHAR(3)); ''')
+    cursor.execute('''  INSERT INTO friends VALUES (1, 'Abhijeet', 32, 'm'); ''')
+
+    #Saving the Actions performed on the DB
+    mysql.connection.commit()
+
+    #Closing the cursor
+    cursor.close()
