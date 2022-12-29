31cce7631806d02f1cdce21281a5190e923855f6
DeVogelRyan <devogelryan2@gmail.com>
2022-12-29 21:21:27 +0100

    changed flask api to db connector

--- app.py
+++ app.py
@@ -1,10 +1,24 @@
-from flask import Flask
-
+from flask import Flask,render_template, request
+from flask_mysqldb import MySQL
+ 
 app = Flask(__name__)
-
-@app.route("/")
-def hello():
-    return "Hello world!"
-
-if __name__ == "__main__":
-    app.run(debug=True, host="0.0.0.0")
+ 
+app.config['MYSQL_HOST'] = 'localhost'
+app.config['MYSQL_USER'] = 'root'
+app.config['MYSQL_PASSWORD'] = ''
+app.config['MYSQL_DB'] = 'mydata'
+ 
+mysql = MySQL(app)
+with app.app_context():
+    #Creating a connection cursor
+    cursor = mysql.connection.cursor()
+    
+    #Executing SQL Statements
+    cursor.execute(''' CREATE TABLE users (name VARCHAR(20) NOT NULL)''')
+    cursor.execute(''' INSERT INTO users VALUES('ryan') ''')
+    
+    #Saving the Actions performed on the DB
+    mysql.connection.commit()
+    
+    #Closing the cursor
+    cursor.close()
